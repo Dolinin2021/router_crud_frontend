3324f8227856633b538ad9b12c620fbdc2fef9bf
Dolinin2021 <programmistdolinin@mail.ru>
2024-01-24 19:55:23 +0600

    modified config files

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://dolinin2021.github.io/router_crud_frontend/',
   plugins: [react()],
 })
